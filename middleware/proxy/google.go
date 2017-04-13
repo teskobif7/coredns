teskobif7/coredns

commit cb9a52c160218c5ca541d77da825e49e89bb83ad

middleware/proxy: Allow non-HTTP upstreams to be health checked (#589)
Allow HTTP health check to be performed against a regular DNS upstream server.

TODO: Add tests.

middleware/proxy/upstream.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
+	"log"
 	"net"
 	"net/http"
+	"net/url"
 	"strconv"
 	"strings"
 	"sync/atomic"
@@ -14,7 +16,6 @@ import (
 	"github.com/coredns/coredns/middleware"
 	"github.com/coredns/coredns/middleware/pkg/dnsutil"
 	"github.com/coredns/coredns/middleware/pkg/tls"
-
 	"github.com/mholt/caddy/caddyfile"
 	"github.com/miekg/dns"
 )
@@ -229,16 +230,38 @@ func parseBlock(c *caddyfile.Dispenser, u *staticUpstream) error {
 
 func (u *staticUpstream) healthCheck() {
 	for _, host := range u.Hosts {
-		port := ""
-		if u.HealthCheck.Port != "" {
-			port = ":" + u.HealthCheck.Port
+		var hostName, checkPort string
+
+		// The DNS server might be an HTTP server.  If so, extract its name.
+		if url, err := url.Parse(host.Name); err == nil {
+			hostName = url.Host
+		} else {
+			hostName = host.Name
 		}
-		hostURL := host.Name + port + u.HealthCheck.Path
+
+		// Extract the port number from the parsed server name.
+		checkHostName, checkPort, err := net.SplitHostPort(hostName)
+		if err != nil {
+			checkHostName = hostName
+		}
+
+		if u.HealthCheck.Port != "" {
+			checkPort = u.HealthCheck.Port
+		}
+
+		hostURL := "http://" + net.JoinHostPort(checkHostName, checkPort) + u.HealthCheck.Path
+		host.Unhealthy = false
+
 		if r, err := http.Get(hostURL); err == nil {
 			io.Copy(ioutil.Discard, r.Body)
 			r.Body.Close()
-			host.Unhealthy = r.StatusCode < 200 || r.StatusCode >= 400
+			if r.StatusCode < 200 || r.StatusCode >= 400 {
+				log.Printf("[WARNING] Health check URL %s returned HTTP code %d\n",
+					hostURL, r.StatusCode)
+				host.Unhealthy = true
+			}
 		} else {
+			log.Printf("[WARNING] Health check probe failed: %v\n", err)
 			host.Unhealthy = true
 		}
 	}

test/proxy_http_health_test.go
@@ -1,0 +1,95 @@
+package test
+
+import (
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+
+	"github.com/coredns/coredns/middleware/proxy"
+	"github.com/coredns/coredns/middleware/test"
+	"github.com/coredns/coredns/request"
+	"github.com/miekg/dns"
+)
+
+func TestProxyWithHTTPCheckOK(t *testing.T) {
+	log.SetOutput(ioutil.Discard)
+
+	healthCheckServer := httptest.NewServer(http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusOK)
+			io.WriteString(w, "OK\n")
+		}))
+	defer healthCheckServer.Close()
+
+	healthCheckURL, err := url.Parse(healthCheckServer.URL)
+	if err != nil {
+		t.Fatal(err)
+	}
+	healthCheckPort := healthCheckURL.Port()
+
+	name, rm, err := test.TempFile(".", exampleOrg)
+	if err != nil {
+		t.Fatalf("failed to create zone: %s", err)
+	}
+	defer rm()
+
+	// We have to bind to 127.0.0.1 because the server started by
+	// httptest.NewServer does, and the IP addresses of the backend
+	// DNS and HTTP servers must match.
+	authoritativeCorefile := `example.org:0 {
+	   bind 127.0.0.1
+       file ` + name + `
+}
+`
+
+	authoritativeInstance, err := CoreDNSServer(authoritativeCorefile)
+	if err != nil {
+		t.Fatalf("Could not get CoreDNS authoritative instance: %s", err)
+	}
+
+	authoritativeAddr, _ := CoreDNSServerPorts(authoritativeInstance, 0)
+	if authoritativeAddr == "" {
+		t.Fatalf("Could not get CoreDNS authoritative instance UDP listening port")
+	}
+	defer authoritativeInstance.Stop()
+
+	proxyCorefile := `example.org:0 {
+    proxy . ` + authoritativeAddr + ` {
+		health_check /health:` + healthCheckPort + ` 1s
+
+	}
+}
+`
+
+	proxyInstance, err := CoreDNSServer(proxyCorefile)
+	if err != nil {
+		t.Fatalf("Could not get CoreDNS proxy instance: %s", err)
+	}
+
+	proxyAddr, _ := CoreDNSServerPorts(proxyInstance, 0)
+	if proxyAddr == "" {
+		t.Fatalf("Could not get CoreDNS proxy instance UDP listening port")
+	}
+	defer proxyInstance.Stop()
+
+	p := proxy.NewLookup([]string{proxyAddr})
+	state := request.Request{W: &test.ResponseWriter{}, Req: new(dns.Msg)}
+	resp, err := p.Lookup(state, "example.org.", dns.TypeA)
+	if err != nil {
+		t.Fatal("Expected to receive reply, but didn't")
+	}
+	// expect answer section with A record in it
+	if len(resp.Answer) == 0 {
+		t.Fatalf("Expected to at least one RR in the answer section, got none: %s", resp)
+	}
+	if resp.Answer[0].Header().Rrtype != dns.TypeA {
+		t.Errorf("Expected RR to A, got: %d", resp.Answer[0].Header().Rrtype)
+	}
+	if resp.Answer[0].(*dns.A).A.String() != "127.0.0.1" {
+		t.Errorf("Expected 127.0.0.1, got: %s", resp.Answer[0].(*dns.A).A.String())
+	}
+}

commit 6eef817ef0f0d3e9ec38b648efda3ef5f232ef0f

middleware/proxy: Make Unhealthy a pointer (#615)
Pointer updates are atomic so drop the sync.RWMutex as it is not needed
anymore. This also fixes the race introduced with cb9a52c (although I
believe this is the first time we properly tested that code path).

middleware/proxy/google.go
@@ -218,11 +218,11 @@ func newUpstream(hosts []string, old *staticUpstream) Upstream {
 			Conns:       0,
 			Fails:       0,
 			FailTimeout: upstream.FailTimeout,
-			Unhealthy:   false,
+			Unhealthy:   newBool(),
 
 			CheckDown: func(upstream *staticUpstream) UpstreamHostDownFunc {
 				return func(uh *UpstreamHost) bool {
-					if uh.Unhealthy {
+					if *uh.Unhealthy {
 						return true
 					}
 

middleware/proxy/lookup.go
@@ -38,10 +38,10 @@ func NewLookupWithOption(hosts []string, opts Options) Proxy {
 			Fails:       0,
 			FailTimeout: upstream.FailTimeout,
 
-			Unhealthy: false,
+			Unhealthy: newBool(),
 			CheckDown: func(upstream *staticUpstream) UpstreamHostDownFunc {
 				return func(uh *UpstreamHost) bool {
-					if uh.Unhealthy {
+					if *uh.Unhealthy {
 						return true
 					}
 					fails := atomic.LoadInt32(&uh.Fails)

middleware/proxy/proxy.go
@@ -3,7 +3,6 @@ package proxy
 
 import (
 	"errors"
-	"sync"
 	"sync/atomic"
 	"time"
 
@@ -57,10 +56,9 @@ type UpstreamHost struct {
 	Name              string // IP address (and port) of this upstream host
 	Fails             int32
 	FailTimeout       time.Duration
-	Unhealthy         bool
+	Unhealthy         *bool
 	CheckDown         UpstreamHostDownFunc
 	WithoutPathPrefix string
-	checkMu           sync.Mutex
 }
 
 // Down checks whether the upstream host is down or not.
@@ -70,7 +68,7 @@ func (uh *UpstreamHost) Down() bool {
 	if uh.CheckDown == nil {
 		// Default settings
 		fails := atomic.LoadInt32(&uh.Fails)
-		return uh.Unhealthy || fails > 0
+		return *uh.Unhealthy || fails > 0
 	}
 	return uh.CheckDown(uh)
 }

middleware/proxy/upstream.go
@@ -84,11 +84,11 @@ func NewStaticUpstreams(c *caddyfile.Dispenser) ([]Upstream, error) {
 				Conns:       0,
 				Fails:       0,
 				FailTimeout: upstream.FailTimeout,
-				Unhealthy:   false,
+				Unhealthy:   newBool(),
 
 				CheckDown: func(upstream *staticUpstream) UpstreamHostDownFunc {
 					return func(uh *UpstreamHost) bool {
-						if uh.Unhealthy {
+						if *uh.Unhealthy {
 							return true
 						}
 
@@ -251,22 +251,19 @@ func (u *staticUpstream) healthCheck() {
 
 		hostURL := "http://" + net.JoinHostPort(checkHostName, checkPort) + u.HealthCheck.Path
 
-		host.checkMu.Lock()
-		defer host.checkMu.Unlock()
-
 		if r, err := http.Get(hostURL); err == nil {
 			io.Copy(ioutil.Discard, r.Body)
 			r.Body.Close()
 			if r.StatusCode < 200 || r.StatusCode >= 400 {
 				log.Printf("[WARNING] Health check URL %s returned HTTP code %d\n",
 					hostURL, r.StatusCode)
-				host.Unhealthy = true
+				*host.Unhealthy = true
 			} else {
-				host.Unhealthy = false
+				*host.Unhealthy = false
 			}
 		} else {
 			log.Printf("[WARNING] Health check probe failed: %v\n", err)
-			host.Unhealthy = true
+			*host.Unhealthy = true
 		}
 	}
 }
@@ -341,3 +338,9 @@ func (u *staticUpstream) IsAllowedDomain(name string) bool {
 }
 
 func (u *staticUpstream) Exchanger() Exchanger { return u.ex }
+
+func newBool() *bool {
+	b := new(bool)
+	*b = false
+	return b
+}

middleware/proxy/policy_test.go
@@ -28,13 +28,16 @@ func (r *customPolicy) Select(pool HostPool) *UpstreamHost {
 func testPool() HostPool {
 	pool := []*UpstreamHost{
 		{
-			Name: workableServer.URL, // this should resolve (healthcheck test)
+			Name:      workableServer.URL, // this should resolve (healthcheck test)
+			Unhealthy: newBool(),
 		},
 		{
-			Name: "http://shouldnot.resolve", // this shouldn't
+			Name:      "http://shouldnot.resolve", // this shouldn't
+			Unhealthy: newBool(),
 		},
 		{
-			Name: "http://C",
+			Name:      "http://C",
+			Unhealthy: newBool(),
 		},
 	}
 	return HostPool(pool)
@@ -54,7 +57,7 @@ func TestRoundRobinPolicy(t *testing.T) {
 		t.Error("Expected second round robin host to be third host in the pool.")
 	}
 	// mark host as down
-	pool[0].Unhealthy = true
+	*pool[0].Unhealthy = true
 	h = rrPolicy.Select(pool)
 	if h != pool[1] {
 		t.Error("Expected third round robin host to be first host in the pool.")

middleware/proxy/upstream_test.go
@@ -42,13 +42,13 @@ func TestSelect(t *testing.T) {
 		FailTimeout: 10 * time.Second,
 		MaxFails:    1,
 	}
-	upstream.Hosts[0].Unhealthy = true
-	upstream.Hosts[1].Unhealthy = true
-	upstream.Hosts[2].Unhealthy = true
+	*upstream.Hosts[0].Unhealthy = true
+	*upstream.Hosts[1].Unhealthy = true
+	*upstream.Hosts[2].Unhealthy = true
 	if h := upstream.Select(); h != nil {
 		t.Error("Expected select to return nil as all host are down")
 	}
-	upstream.Hosts[2].Unhealthy = false
+	*upstream.Hosts[2].Unhealthy = false
 	if h := upstream.Select(); h == nil {
 		t.Error("Expected select to not return nil")
 	}